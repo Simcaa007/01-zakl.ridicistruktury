e8972d17f24ddb7bde54b56f7b18cd81817eca63
Simcaa007 <havelkova.si@seznam.cz>
2026-01-20 09:38:12 +0100

    Implement user input and loop selection for summation
    Added functionality to prompt user for input and cycle selection for summing a sequence of random numbers.

--- 02/02.cpp
+++ 02/02.cpp
@@ -1,20 +1,57 @@
-﻿// 02.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+// 02.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
 //
 
 #include <iostream>
+#include <ctime>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    cout << "Pro začátek zadejte 1: ";
+    int zacatek;
+    cin >> zacatek;
+
+    while (zacatek == 1)
+    {
+        int n;
+        cout << "Zadejte cele cislo n: ";
+        cin >> n;
+
+        cout << "\nZadejte volbu cyklu: while - w, do while - d, for - f: ";
+        char volba;
+        cin >> volba;
+
+        for (int i = 0; i <= n; i++)
+        {
+            
+        }
+
+        if (volba == 'w') 
+        {
+
+        }
+
+        if (volba == 'd')
+        {
+        }
+
+        if (volba == 'f')
+        {
+        }
+
+        continue;
+    }
+
+    
 }
 
 // Spuštění programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladění
 // Ladění programu: F5 nebo nabídka Ladit > Spustit ladění
 
-// Tipy pro zahájení práce:
-//   1. K přidání nebo správě souborů použijte okno Průzkumník řešení.
-//   2. Pro připojení ke správě zdrojového kódu použijte okno Team Explorer.
-//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
-//   4. K zobrazení chyb použijte okno Seznam chyb.
-//   5. Pokud chcete vytvořit nové soubory kódu, přejděte na Projekt > Přidat novou položku. Pokud chcete přidat do projektu existující soubory kódu, přejděte na Projekt > Přidat existující položku.
-//   6. Pokud budete chtít v budoucnu znovu otevřít tento projekt, přejděte na Soubor > Otevřít > Projekt a vyberte příslušný soubor .sln.
+// C++ Na vstupu je dáno celé číslo n. 
+// Vygenerujte n-prvkovou posloupnost náhodných reálných čísel z intervalu <10;100). 
+// Určete a vypište součet prvků posloupnosti. Nechte uživatele zvolit, jakým typem cyklu bude součet spočítán – while, 
+// do while a for. Nabídka se bude cyklicky opakovat, dokud uživatel nezadá
+//čtvrtou volbu – KONEC.
