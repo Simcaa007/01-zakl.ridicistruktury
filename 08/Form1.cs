ddf9aed4b1c30e41b1bd7b8daf79f466b684d345
Simcaa007 <havelkova.si@seznam.cz>
2026-01-31 19:57:35 +0100

    Hotovy priklad 8

---
+++ 08/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vys = "";
+            foreach(string line in textBox1.Lines)
+            {
+                if (checkBox1.Checked && checkBox2.Checked)
+                {
+                    vys += line.Substring(0, 1).ToUpper() + line.Substring(1, line.Length - 1) + "." + Environment.NewLine;
+                }
+                else if (checkBox1.Checked) {
+                    vys += line.Substring(0, 1).ToUpper() + line.Substring(1, line.Length - 1) + Environment.NewLine;
+                }
+                else if (checkBox2.Checked)
+                {
+                    vys += line + Environment.NewLine;
+                }
+            }
+
+            textBox1.Clear();
+
+            textBox1.Text = vys;
+        }
+    }
+}
